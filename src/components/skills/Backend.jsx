0c8b297a9cffab5e5d8bd12ab591a2d9cc600e57
Victor Del Carpio <victor.delcarpiogomez@yale.edu>
2023-03-20 16:04:33 -0400

    skills section

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">
+            Backend Developer
+        </h3>
+        <div className="skills__box">
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bxl-python' ></i>
+                    <div>
+                        <h3 className="skills__name">Python</h3>
+                        <span className="skills__level">6+ Years</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-java' ></i>
+                    <div>
+                        <h3 className="skills__name">Java</h3>
+                        <span className="skills__level">7 Years</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-c-plus-plus' ></i>
+                    <div>
+                        <h3 className="skills__name">C/C++</h3>
+                        <span className="skills__level">3 Years</span>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bx-terminal'></i>
+                    <div>
+                        <h3 className="skills__name">Unix</h3>
+                        <span className="skills__level">4 Years</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxs-data'></i>
+                    <div>
+                        <h3 className="skills__name">SQL</h3>
+                        <span className="skills__level">2 Years</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-stats'></i>
+                    <div>
+                        <h3 className="skills__name">R</h3>
+                        <span className="skills__level">4 Years</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Backend;
---
+++ src/components/skills/Frontend.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Frontend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">
+            Frontend Developer
+        </h3>
+        <div className="skills__box">
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bxl-html5'></i>
+                    <div>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-css3' ></i>
+                    <div>
+                        <h3 className="skills__name">CSS</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-javascript' ></i>
+                    <div>
+                        <h3 className="skills__name">JavaScript</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bxl-git' ></i>
+                    <div>
+                        <h3 className="skills__name">Git</h3>
+                        <span className="skills__level">Advanced</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-react' ></i>
+                    <div>
+                        <h3 className="skills__name">React</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bxl-flask'></i>
+                    <div>
+                        <h3 className="skills__name">Flask</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Frontend;
