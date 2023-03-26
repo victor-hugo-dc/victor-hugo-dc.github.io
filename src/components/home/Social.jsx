f740dafd289b589783cb2d83a452d070c3dfcc8e
Victor Del Carpio <victor.delcarpiogomez@yale.edu>
2023-03-25 23:26:14 -0400

    minor changes

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/victordelcarpio/" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="https://github.com/victor-hugo-dc" className="home__social-icon" target="_blank">
             <i className="uil uil-github-alt"></i>
         </a>
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/victor-hugo-dc/" className="home__social-icon" target="_blank">
             <i class="uil uil-linkedin-alt"></i>
         </a>
     </div>
