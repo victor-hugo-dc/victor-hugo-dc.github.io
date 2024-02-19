16ea4bfd1d0056313052eaebb5683e2a424423ca
Victor Hugo <victor.hugo.dcg@gmail.com>
2024-02-18 23:02:25 -0500

    added memoji, spin icons on hover

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import About from './components/about/About';
-import Header from './components/header/Header';
 import Home from './components/home/Home';
-import Skills from './components/skills/Skills';
-import Qualification from './components/qualification/Qualification';
-import Contact from './components/contact/Contact';
-// import Footer from './components/footer/Footer';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>        
-    
-    <BrowserRouter>
-
-      <Header/>
+    <>
       <main className='main'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/portfolio" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/qualification" element={<Qualification />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+        <Home />
       </main>
-      {/* <Footer/> */}
-    </BrowserRouter>
-
     </>
   );
 }
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/victordelcarpio/" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/victordelcarpio/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
-        <a href="https://github.com/victor-hugo-dc" className="home__social-icon" target="_blank">
+        <a href="https://github.com/victor-hugo-dc" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-github-alt"></i>
         </a>
-        <a href="https://www.linkedin.com/in/victor-hugo-dc/" className="home__social-icon" target="_blank">
-            <i class="uil uil-linkedin-alt"></i>
+        <a href="https://www.linkedin.com/in/victor-hugo-dc/" className="home__social-icon" target="_blank" rel="noreferrer">
+            <i className="uil uil-linkedin-alt"></i>
         </a>
     </div>
   );
