22872228cf501dd28aa2a80f8968298320eadf64
Victor Hugo <victor.hugo.dcg@gmail.com>
2024-12-16 13:51:10 -0500

    feat: update Dec 2024

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -6,7 +6,7 @@ const Data = () => {
         <h1 className="home__title">Victor Hugo</h1>
         <h3 className="home__subtitle">Software Engineer</h3>
         <p className="home__description">
-            I'm a Software Engineering at Luminous Delivery. Previously at Snapchat and Microsoft. Former Computer Science student at Yale University.
+            Building websites at <a href='https://javiwebdesign.com/' target="_blank" rel="noreferrer">Javi</a>. Previously at Snapchat and Microsoft. Former Computer Science student at Yale University.
         </p>
     </div>
   );
