949ec0459cc4e4c4a6ff0fb4be27b3b866f05c3b
Victor Del Carpio <vhdelcg@gmail.com>
2025-10-04 18:14:46 -0400

    fix: typo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,11 +22,11 @@ const Home = () => {
                 </h1>
                 <div className="mb-8 prose leading-6 text-justify text-gray-600 dark:text-gray-400">
                     <p>
-                        I&apos;m a Peruvian-American software engineer based in Northern Virginia. I prefer backend work, specializing in cloud systems. Currently, I&apos;m building <a target="_blank" rel="noreferrer" href="https://javiwebdesign.com">Capitol Creative Collective</a>. Check out my latest projects for <a target="_blank" rel="noreferrer" href="https://www.instagram.com/atlanticsuncontrol_inc/">Atlantic Sun Control, Inc.</a> and <a target="_blank" rel="noreferrer" href="https://allenporterlandscaping.com/"></a>Allen Porter Landscape Services.
+                        I&apos;m a Peruvian-American software engineer based in Northern Virginia. I prefer backend work, specializing in cloud systems. Currently, I&apos;m building <a target="_blank" rel="noreferrer" href="https://javiwebdesign.com">Capitol Creative Collective</a>. Check out my latest projects for <a target="_blank" rel="noreferrer" href="https://www.instagram.com/atlanticsuncontrol_inc/">Atlantic Sun Control, Inc.</a> and <a target="_blank" rel="noreferrer" href="https://allenporterlandscaping.com/">Allen Porter Landscape Services</a>.
                         <br /><br />
                         As a former QuestBridge Scholar at Yale University, I&apos;ve had the opportunity to intern at companies like <a target="_blank" rel="noreferrer" href="https://www.microsoft.com/en-us/">Microsoft</a> and <a target="_blank" rel="noreferrer" href="https://www.snapchat.com/">Snap, Inc.</a>, as well as contribute to research at the <a target="_blank" rel='noreferrer' href='https://interactive-machines.com/'>Interactive Machines group</a>. I&apos;ve since transferred to James Madison University, where I expect to graduate in May 2026.
                         <br /><br />
-                        AFK, I enjoy the outdoors: camping, hiking, fishing. I&apos;ve recently picked up tennis and have previously trained Muay Thai. Creatively, I like to express myself with cameras and charcoal. I recently fixed up my dad&apos;s retro Palmcorder and have been collecting memories on cassette tapes. Check out my résumé.
+                        AFK, I enjoy the outdoors: camping, hiking, fishing. I&apos;ve recently picked up tennis and have previously trained Muay Thai. Creatively, I like to express myself with cameras and charcoal. I recently fixed up my dad&apos;s retro Palmcorder and have been collecting memories on cassette tapes.
                         <br /><br />
                         Check out my <a href='assets/victor-hugo-dc.pdf'>r&eacute;sum&eacute;</a>.
                     </p>
